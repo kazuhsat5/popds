9e93b820d8041fd3babd7bcf49435edeeec74572
佐藤　和大 <kazuhsat@MBP-15JAC-671.local>
2015-10-17 01:28:01 +0900

    クラス変数削除

--- src/Element/Xml.php
+++ src/Element/Xml.php
@@ -24,20 +24,12 @@ class Xml extends Element
     private $_encoding = 'utf-8';
 
     /**
-     * 子要素
-     *
-     * @var
-     */
-    private $_elements = [];
-
-    /**
      * 出力する
      *
      * @return string
      */
     public function output()
     {
-        var_dump('test');
         return sprintf('<?xml version="%s" encoding="%s"?>', $this->_version, $this->_encoding);
     }
 
@@ -49,16 +41,5 @@ class Xml extends Element
     public function setEncoding($encoding)
     {
         $this->_encoding = $encoding;
-    }
-
-    /**
-     * 子要素を追加する
-     *
-     * @param Popds\Element 子要素
-     * @return void
-     */
-    public function addElement(Element $element)
-    {
-        $this->_elements[] = $element;
     }
 }
