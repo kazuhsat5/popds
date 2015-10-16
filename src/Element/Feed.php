2923503bd17677e1c591f0622922f44e04242948
佐藤　和大 <kazuhsat@MBP-15JAC-671.local>
2015-10-17 01:46:46 +0900

    feedクラス追加

---
+++ src/Element/Feed.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Popds\Element;
+
+/**
+ * feedクラス
+ *
+ * @author kazuhsat <kazuhsat555@gmail.com>
+ */
+class Feed extends Element
+{
+    /**
+     * xmlns属性値
+     *
+     * @var
+     */
+    private $_xmlns = 'http://www.w3.org/2005/Atom';
+
+    /**
+     * xml:lang属性値
+     *
+     * @var
+     */
+    private $_lang = 'ja-JP';
+
+    /**
+     * 子要素
+     *
+     * @var
+     */
+    private $_elements = [];
+
+    /**
+     * 出力
+     *
+     * @return string
+     */
+    public function output()
+    {
+        $result = '';
+        $result .= sprintf('<feed xmlns="%s" xml:lang="%s">', $this->_xmlns, $this->_lang);
+        $result .= '</feed>'
+    }
+
+    public function setXmlns($xmlns)
+    {
+        $this->_xmlns = $xmlns;
+    }
+
+    public function setLang($lang)
+    {
+        $this->_lang = $lang;
+    }
+
+    /**
+     * 子要素を追加する
+     *
+     * @param Popds\Element $element
+     * @return void
+     */
+    public function addElement($element)
+    {
+        $this->_elements[] = $element;
+    }
+}
